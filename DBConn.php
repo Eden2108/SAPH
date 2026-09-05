968f5f3421d43ddbe17e4f47cc55e953549bf1a2
Eden2108 <ST10452860@rcconnect.edu.za>
2026-09-05 02:48:22 +0200

    Update database connection settings for deployment environment

--- DBConn.php
+++ DBConn.php
@@ -1,12 +1,12 @@
 <?php
-$servername = "localhost";
-$username   = "root";
-$password   = "";
-$dbname     = "save_a_pet_hub"; // <-- actual database name in phpMyAdmin
+$host = "sql104.infinityfree.com";   // InfinityFree host
+$user = "if0_42835585";      // Our DB username
+$password = "VGD8qrba9GAvG";  // Our DB password
+$dbname = "if0_42835585_save_a_pet_hub";  // Our DB name
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($host, $user, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 ?>
